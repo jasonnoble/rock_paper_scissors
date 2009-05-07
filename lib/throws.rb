de1a7531756138fbd9810438778adaeb9f1fb177
Jason Noble <perlwizard@gmail.com>
2009-05-07 15:14:40 -0400

    Remoted Throws class

--- lib/throws.rb
+++ lib/throws.rb
@@ -1,26 +1,18 @@
-class Throw
-  include Comparable
-  
-  def Throw.beats?(other)
-    true
-  end
-end
-
-class Scissors < Throw
+class Scissors
   def Scissors.beats?(other)
     return true if other == Paper
     false
   end
 end
 
-class Paper < Throw
+class Paper
   def Paper.beats?(other)
     return true if other == Rock
     false
   end
 end
 
-class Rock < Throw
+class Rock
   def Rock.beats?(other)
     return true if other == Scissors
     false
